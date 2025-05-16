7a8e66d74300030b44ee1035fcdaea048e73f35f
Sivaram <sivaramm683@gmail.com>
2025-05-16 14:35:39 +0530

    change in about

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export const Header = () => {
         <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
           <a href="#home" className="nav-link">Home</a>
           <a href="#about" className="nav-link">About</a>
-           <a href="#skills" className="nav-link">Skills</a>
+           <a href="#skills-and-tools" className="nav-link">Skills</a>
           <a href="#projects" className="nav-link">Projects</a>
           <a href="#certifications" className="nav-link">Certifications</a>
           <a href="#contact" className="nav-link">Contact</a>
