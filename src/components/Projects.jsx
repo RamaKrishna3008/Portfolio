093d0220738aac7b37ba1c3b59e49e474e2bb2f4
Sivaram <sivaramm683@gmail.com>
2025-07-27 16:15:35 +0530

    updated portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,23 +6,36 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export const Projects = () => {
   const projects = [
+   
     {
+      title: "Movie Ticket Booking Platform",
+      description: "MovieTicketBookingPlatform is a full-stack web application for booking movie tickets, featuring role-based access for users, theatre owners, and admins. It integrates Razorpay for secure payments and uses AWS S3 Client to manage and store multimedia assets like posters and banners, ensuring a seamless and scalable movie booking experience.",
+      technologies: ["Springboot", "Java", "React", "MySQL"],
+      githubLink: "https://github.com/RamaKrishna3008/MovieTicketBookingPlatform"
+    },
+     {
       title: "Enterprise Resource Planning For a University",
-      description: "An ERP system built with Spring Boot, integrated with Razorpay for secure and efficient fee payments.",
+      description: "A comprehensive ERP system developed using Spring Boot, designed to manage student information, academics, and administration efficiently. Integrated with Razorpay for secure fee payments. It ensures smooth operations for universities by providing different modules . ",
       technologies: ["Springboot", "Java", "JSP", "MySQL"],
       githubLink: "https://github.com/yourusername/project1"
     },
     {
+      title: "Chat App",
+      description: "A real-time Chat Application built using Spring Boot and React, leveraging WebSocket and STOMP.js for instant messaging. MongoDB is used to persist user data and chat logs. The system enables seamless one-to-one communication with efficient backend message routing and UI updates without page reloads.",
+      technologies: ["Springboot", "Java", "React", "MongoDb"],
+      githubLink: "https://github.com/RamaKrishna3008/chatApp-Backend"
+    },
+    {
       title: "Ecommerce Project with Springboot and JSP",
-      description: "An eCommerce application built with Spring Boot and JSP for seamless online shopping experiences.",
+      description: "An eCommerce platform developed using Spring Boot and JSP, offering product browsing, cart management, and checkout functionalities. Integrated with MySQL for persistent product and user data. Designed for responsiveness and ease of use, the system supports basic CRUD operations and secure order processing.",
       technologies: ["Springboot", "Java", "JSP", "MySQL"],
       githubLink: "https://github.com/RamaKrishna3008/Ecommerce_JSP"
     },
     {
       title: "Placement And Activity Management System",
-      description: "A Placement Management System developed using Java EE beans and JSF to streamline student placement process.",
+      description: "A Placement Management System developed using Java EE, JSF, and Servlets, aimed at simplifying the student placement lifecycle. It allows faculty and placement officers to manage student profiles, job postings, and applications efficiently. MySQL is used as the backend database to ensure reliable data storage and retrieval.",
       technologies: ["Java EE", "Servlets", "JSF","MySQL"],
-      githubLink: "https://github.com/yourusername/project3"
+      githubLink: "https://github.com/RamaKrishna3008/Placement-Management-"
     }
   ];
 
